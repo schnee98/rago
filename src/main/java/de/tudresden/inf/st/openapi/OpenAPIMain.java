d5cb7f0c2cd3337cb27897be1656cd1006065aaa
Jueun Park <jooeun06161@gmail.com>
2022-02-23 08:17:24 +0900

    edit

--- src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
+++ src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
@@ -6,8 +6,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import static de.tudresden.inf.st.openapi.ast.ASTNode.assertNotNull;
-
 public class OpenAPIMain {
 
   /**
