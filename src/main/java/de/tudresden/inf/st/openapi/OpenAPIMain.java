84d34ecc7bbba63da2e1548fa7a0de85cbecff6e
Jueun Park <s5616012@mailbox.tu-dresden.de>
2022-02-23 07:55:11 +0900

    add yaml for oas ver 3.1, processed relast

--- src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
+++ src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
@@ -1,17 +1,12 @@
 package de.tudresden.inf.st.openapi;
 
-import de.tudresden.inf.st.openapi.ast.OpenAPIObject;
-import org.openapi4j.core.validation.ValidationResults;
-import org.openapi4j.parser.OpenApi3Parser;
-import org.openapi4j.parser.model.v3.OpenApi3;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import static de.tudresden.inf.st.openapi.ast.ASTNode.assertNotNull;
 
 public class OpenAPIMain {
 
@@ -19,9 +14,6 @@ public class OpenAPIMain {
    * main-method, calls the set of methods to test the OpenAPI-Generator with JastAdd
    **/
   public static void main(String[] args) throws Exception {
-    OpenAPIObject openApi = new OpenAPIObject();
-    OpenApi3 api3;
-    ValidationResults results;
     List<String> filenames = new ArrayList<>();
     String genDir = "./gen-api-ex/";
     File genDirectory = new File(genDir);
@@ -29,27 +21,10 @@ public class OpenAPIMain {
 
     File resource = new File("./src/main/resources");
 
-    for (File file : resource.listFiles())
-      filenames.add(file.getName());
-    System.out.println(filenames.size());
 
-        /*
-        for( String file : filenames ){
-            String writerName = genDir + file;
+    String fileName = "./src/main/resources/3.1/basicref.yaml";
 
-            URL expUrl = OpenAPIMain.class.getClassLoader().getResource(file);
-            OpenApi3 api = new OpenApi3Parser().parse(expUrl, new ArrayList<>(), true);
-            System.out.println("Loading expression DSL file '" + file + "'.");
 
-            openApi = OpenAPIObject.parseOpenAPI(api);
-            api3 = OpenAPIObject.composeOpenAPI(openApi);
-            openApi.generateRequests();
-
-        }
-         */
-
-    String fileName = "petstore-v2.yaml";
-    //FileWriter writer = new FileWriter("./gen-api-ex/callback-example_generated.json");
 
     URL expUrl = OpenAPIMain.class.getClassLoader().getResource(fileName);
     File file = null;
@@ -62,17 +37,7 @@ public class OpenAPIMain {
       throw new FileNotFoundException("Could not load JSON file " + fileName);
     }
 
-    OpenApi3 api = new OpenApi3Parser().parse(expUrl, new ArrayList<>(), true);
     System.out.println("Loading expression DSL file '" + fileName + "'.");
-
-    //openApi = OpenAPIObject.parseOpenAPI(api);
-    //openApi.generateRequestsWithInferredParameters();
-
-    //writer.write(api3.toNode().toPrettyString());
-    //writer.close();
-
-
-    Map<String, List<String>> s = new HashMap<>();
 
     if (args.length > 0) {
       fileName = args[0];
