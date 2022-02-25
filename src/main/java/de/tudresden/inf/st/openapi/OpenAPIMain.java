0e4f6e50cb217a808fad2e7579b23444668c8abf
Jueun Park <s5616012@mailbox.tu-dresden.de>
2022-02-25 09:14:47 +0900

    edit parser (~OpenAPIObject)

--- src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
+++ src/main/java/de/tudresden/inf/st/openapi/OpenAPIMain.java
@@ -1,5 +1,12 @@
 package de.tudresden.inf.st.openapi;
 
+import io.swagger.models.reader.SwaggerParser;
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.util.Yaml;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
@@ -16,11 +23,19 @@ public class OpenAPIMain {
     String genDir = "./gen-api-ex/";
     File genDirectory = new File(genDir);
     File[] contents;
-
     File resource = new File("./src/main/resources");
 
+    // init parser
+    String fileName = "./src/main/resources/3.1/basicOAS31.yaml";
+    ParseOptions options = new ParseOptions();
+    options.setResolve(true);
+    options.setResolveFully(true);
+    options.setAllowEmptyString(false);
+    SwaggerParseResult result = new OpenAPIParser().readLocation(fileName, null, options);
+    OpenAPI openAPI = result.getOpenAPI();
 
-    String fileName = "./src/main/resources/3.1/basicref.yaml";
+    // Yaml String
+    System.out.println(Yaml.mapper().writerWithDefaultPrettyPrinter().writeValueAsString(openAPI));
 
 
 
